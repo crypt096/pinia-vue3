664d814b82ec26c9f2c5979d9f8691f91f6cf3a3
Alex <31933549+crypt096@users.noreply.github.com>
2024-07-02 18:56:14 +0200

    Update TaskStore.ts

--- src/stores/TaskStore.ts
+++ src/stores/TaskStore.ts
@@ -1,3 +1,4 @@
+import type { Task } from '@/interfaces/task.interface'
 import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskStore', {
@@ -5,7 +6,19 @@ export const useTaskStore = defineStore('taskStore', {
     tasks: [
       {id: 1, title: "buy some milk", isFav: false},
       {id: 2, title: "play Gloomhaven", isFav: true}
-    ],
-    name: 'Mario'
-  })
+    ]
+  }),
+  getters: {
+    favs(): Task[] {
+      return this.tasks.filter(t => t.isFav)
+    },
+    favCount(): number {
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p
+      }, 0)
+    },
+    totalCount: (state) => {
+      return state.tasks.length
+    }
+  }
 })
