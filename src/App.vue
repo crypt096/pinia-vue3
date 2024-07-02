b1c32b1fb97a3d6838cb02040130f695cfb4bd5a
Alex <31933549+crypt096@users.noreply.github.com>
2024-07-02 18:40:20 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  Hello World
+  <main>
+
+    <!-- Heading -->
+     <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia tasks</h1>
+     </header>
+
+  </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
